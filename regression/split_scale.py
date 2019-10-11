923d7845fb1f13692b9b7e01255c4ac2be8820b2
cariholmes <holmes.l.cari@gmail.com>
2019-10-10 21:20:36 -0500

    df split into 4

--- regression/split_scale.py
+++ regression/split_scale.py
@@ -20,33 +20,75 @@ import env
 from sklearn.model_selection import train_test_split
 
 df = wrangle_telco()
-df.info()
-df.describe()
+# df.info()
+# df.describe()
 
-
-print(train.shape)
-print(test.shape)
+# print(train.shape)
+# print(test.shape)
 
 from sklearn.preprocessing import StandardScaler, QuantileTransformer, PowerTransformer, RobustScaler, MinMaxScaler
-# split_my_data(X, y, train_pct)
 
 X = df.drop(columns=['customer_id', 'total_charges'])
-X
-y = df.total_charges
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=.8, random_state=123)
-X_train, X_test, y_train, y_test
-
-# standard_scaler()
+y = pd.DataFrame(df['total_charges'])
+# type(y)
 
 
+def split_my_data(X, y):
+    X = df.drop(columns=['customer_id', 'total_charges'])
+    y = pd.DataFrame(df['total_charges'])
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=.8, random_state=123)
+    return X_train, X_test, y_train, y_test
 
-# scale_inverse()
 
-# uniform_scaler()
+def standard_scaler(X_train, X_test, y_train, y_test):
+    scalerX = StandardScaler().fit(X_train)
+    scalery = StandardScaler().fit(y_train) 
+    train_scaled_X = pd.DataFrame(scalerX.transform(X_train), columns=X_train.columns.values).set_index([X_train.index.values])
+    train_scaled_y = pd.DataFrame(scalery.transform(y_train), columns=y_train.columns.values).set_index([y_train.index.values])
+    test_scaled_X = pd.DataFrame(scalerX.transform(X_test), columns=X_test.columns.values).set_index([X_test.index.values])
+    test_scaled_y = pd.DataFrame(scalery.transform(y_test), columns=y_test.columns.values).set_index([y_test.index.values])
+    return scalerX, scalery, train_scaled_X, train_scaled_y, test_scaled_X, test_scaled_y
 
-# gaussian_scaler()
+def scale_inverse(scalerX, scalery, train_scaled_X, train_scaled_y, test_scaled_X, test_scaled_y):
+    unscaledX = pd.DataFrame(scalerX.inverse_transform(train_scaled_X), columns=train_scaled_X.columns.values).set_index([X_train.index.values])
+    unscaledy = pd.DataFrame(scalery.inverse_transform(train_scaled_y), columns=train_scaled_y.columns.values).set_index([y_train.index.values])
+    test_X_unscaled = pd.DataFrame(scalerX.inverse_transform(test_scaled_X), columns=test_scaled_X.columns.values).set_index([X_test.index.values])
+    test_y_unscaled = pd.DataFrame(scalery.inverse_transform(test_scaled_y), columns=test_scaled_y.columns.values).set_index([y_test.index.values])
+    return unscaledX, unscaledy, test_X_unscaled, test_y_unscaled
 
-# min_max_scaler()
+def uniform_scaler(X_train, X_test, y_train, y_test):
+    scalerX = QuantileTransformer(n_quantiles=100, output_distribution='uniform', random_state=123, copy=True).fit(X_train)
+    scalery = QuantileTransformer(n_quantiles=100, output_distribution='uniform', random_state=123, copy=True).fit(y_train)
+    train_uniform_X = pd.DataFrame(scalerX.transform(X_train), columns=X_train.columns.values).set_index([X_train.index.values])
+    train_uniform_y = pd.DataFrame(scalery.transform(y_train), columns=y_train.columns.values).set_index([y_train.index.values])
+    test_uniform_X = pd.DataFrame(scalerX.transform(X_test), columns=X_test.columns.values).set_index([X_test.index.values])
+    test_uniform_y = pd.DataFrame(scalery.transform(y_test), columns=y_test.columns.values).set_index([y_test.index.values])
+    return scalerX, scalery, train_uniform_X, train_uniform_y, test_uniform_X, test_uniform_y
 
-# iqr_robust_scaler()
+def gaussian_scaler(X_train, X_test, y_train, y_test):
+    scalerX = PowerTransformer(method='yeo-johnson', standardize=False, copy=True).fit(X_train)
+    scalery = PowerTransformer(method='yeo-johnson', standardize=False, copy=True).fit(y_train)
+    train_yeo_X = pd.DataFrame(scalerX.transform(X_train), columns=X_train.columns.values).set_index([X_train.index.values])
+    train_yeo_y = pd.DataFrame(scalery.transform(y_train), columns=y_train.columns.values).set_index([y_train.index.values])
+    test_yeo_X = pd.DataFrame(scalerX.transform(X_test), columns=X_test.columns.values).set_index([X_test.index.values])
+    test_yeo_y = pd.DataFrame(scalery.transform(y_test), columns=y_test.columns.values).set_index([y_test.index.values])
+    return scalerX, scalery, train_yeo_X, train_yeo_y, test_yeo_X, test_yeo_y
+
+def min_max_scaler(X_train, X_test, y_train, y_test):
+    scalerX = MinMaxScaler(copy=True, feature_range=(0,1)).fit(X_train)
+    scalery = MinMaxScaler(copy=True, feature_range=(0,1)).fit(y_train)
+    train_minmax_X = pd.DataFrame(scalerX.transform(X_train), columns=X_train.columns.values).set_index([X_train.index.values])
+    train_minmax_y = pd.DataFrame(scalery.transform(y_train), columns=y_train.columns.values).set_index([y_train.index.values])
+    test_minmax_X = pd.DataFrame(scalerX.transform(X_test), columns=X_test.columns.values).set_index([X_test.index.values])
+    test_minmax_y = pd.DataFrame(scalery.transform(y_test), columns=y_test.columns.values).set_index([y_test.index.values])
+    return scalerX, scalery, train_minmax_X, train_minmax_y, test_minmax_X, test_minmax_y
+
+def iqr_robust_scaler(X_train, X_test, y_train, y_test):
+    scalerX = RobustScaler(quantile_range=(25.0,75.0), copy=True, with_centering=True, with_scaling=True).fit(X_train)
+    scalery = RobustScaler(quantile_range=(25.0,75.0), copy=True, with_centering=True, with_scaling=True).fit(y_train)
+    train_iqr_X = pd.DataFrame(scalerX.transform(X_train), columns=X_train.columns.values).set_index([X_train.index.values])
+    train_iqr_y = pd.DataFrame(scalery.transform(y_train), columns=y_train.columns.values).set_index([y_train.index.values])
+    test_iqr_X = pd.DataFrame(scalerX.transform(X_test), columns=X_test.columns.values).set_index([X_test.index.values])
+    test_iqr_y = pd.DataFrame(scalery.transform(y_test), columns=y_test.columns.values).set_index([y_test.index.values])
+    return scalerX, scalery, train_iqr_X, train_iqr_y, test_iqr_X, test_iqr_y
+
