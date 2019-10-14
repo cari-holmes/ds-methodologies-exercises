ec7fa22f4ad9dee6fec825b6c8c0d703f78477d4
cariholmes <holmes.l.cari@gmail.com>
2019-10-14 12:06:36 -0500

    working on last problems

--- regression/evaluate.py
+++ regression/evaluate.py
@@ -3,7 +3,6 @@
 from pydataset import data 
 import pandas as pd
 import numpy as np
-%matplotlib inline
 import matplotlib.pyplot as plt
 from scipy import stats
 import seaborn as sns
@@ -38,39 +37,44 @@ regr.summary()
 def plot_residuals(x, y, df):
     return sns.residplot(x, y, df)
 
+plot_residuals(x, y, df)
+
 # Write a function, regression_errors(y, yhat), that takes in y and yhat, returns the sum of squared errors (SSE), explained sum of squares (ESS), total sum of squares (TSS), mean squared error (MSE) and root mean squared error (RMSE).
 
-# df['residual'] = df['yhat'] - y
-# df['residual_2'] = df['residual']**2
-
-# def regression_errors(y, df.yhat):
-#     SSE = sum(df['residual_2'])
-#     MSE = SSE/len(df)
-#     RMSE = sqrt(MSE)
-#     ESS = sum(df['yhat'] - (y).mean()**2)
-#     TSS = SSE + ESS
-#     return SSE, MSE, RMSE, ESS, TSS
-
-def regression_errors(y, df.yaht):
+def regression_errors(y, yhat):
     MSE = mean_squared_error(y, df.yhat)
     SSE = MSE*len(df)
     RMSE = sqrt(MSE)
-    ESS = sum((df.yhat - df.y.mean())**2)
+    ESS = sum((df.yhat - y.mean())**2)
     TSS = SSE + ESS
     return MSE, SSE, RMSE, ESS, TSS
 
 regression_errors(y, df.yhat)
 
-type(df.yhat)
-
 # Write a function, baseline_mean_errors(y), that takes in your target, y, computes the SSE, MSE & RMSE when yhat is equal to the mean of all y, and returns the error values (SSE, MSE, and RMSE).
+## fix this baseline
 
+def baseline_mean_errors(y):
+    yhat = y.mean()
+    df['residual'] = df['yhat'] - y
+    df['residual_2'] = df['residual']**2
+    SSE = sum(df['residual_2'])
+    MSE = SSE/len(df)
+    RMSE = sqrt(MSE)
+    return SSE, MSE, RMSE
 
+baseline_mean_errors(y)
 
 # Write a function, better_than_baseline(SSE), that returns true if your model performs better than the baseline, otherwise false.
 
+def better_than_baseline(SSE):
+    return MSE < mse_base
 
 
 # Write a function, model_significance(ols_model), that takes the ols model as input and returns the amount of variance explained in your model, and the value telling you whether the correlation between the model and the tip value are statistically significant.
+
+def model_significance(ols_model):
+    
+
 
 
