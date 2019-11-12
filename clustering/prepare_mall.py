e02bb1cad7d851c6f519e9d2b862e1956c2b0ec2
cariholmes <holmes.l.cari@gmail.com>
2019-11-11 20:30:19 -0600

    prepare functions for mall data

---
+++ clustering/prepare_mall.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
+import scipy.stats as stats
+import env
+
+
+# Upper Outliers
+
+def get_upper_outliers(s, k):
+    '''
+    Given a series and a cutoff value, k, returns the upper outliers for the
+    series.
+    The values returned will be either 0 (if the point is not an outlier), or a
+    number that indicates how far away from the upper bound the observation is.
+    '''
+    q1, q3 = s.quantile([.25, .75])
+    iqr = q3 - q1
+    upper_bound = q3 + (k * iqr)
+    return s.apply(lambda x: max([x - upper_bound, 0]))
+
+def add_upper_outlier_columns(df, k):
+    '''
+    Add a column with the suffix _outliers for all the numeric columns
+    in the given dataframe.
+    '''
+    # outlier_cols = {col + '_outliers': get_upper_outliers(df[col], k)
+    #                 for col in df.select_dtypes('number')}
+    # return df.assign(**outlier_cols)
+    for col in df.select_dtypes('number'):
+        df[col + '_outliers'] = get_upper_outliers(df[col], k)
+    return df
+
+# Split data into train and test
+
+def split_my_data(df, train_pct=.80, random_state=123):
+    train, test = train_test_split(df, train_size=train_pct, random_state=random_state)
+    return train, test
+
+# Encode data as needed for modeling
+
+def encode(train, test, col_name):
+    encoded_values = sorted(list(train[col_name].unique()))
+    # Integer Encoding
+    int_encoder = LabelEncoder()
+    train.encoded = int_encoder.fit_transform(train[col_name])
+    test.encoded = int_encoder.transform(test[col_name])
+    # create 2D np arrays of the encoded variable (in train and test)
+    train_array = np.array(train.encoded).reshape(len(train.encoded),1)
+    test_array = np.array(test.encoded).reshape(len(test.encoded),1)
+    # One Hot Encoding
+    ohe = OneHotEncoder(sparse=False, categories='auto')
+    train_ohe = ohe.fit_transform(train_array)
+    test_ohe = ohe.transform(test_array)
+    # Turn the array of new values into a data frame with columns names being the values
+    # and index matching that of train/test
+    # then merge the new dataframe with the existing train/test dataframe
+    train_encoded = pd.DataFrame(data=train_ohe,
+                            columns=encoded_values, index=train.index)
+    train = train.join(train_encoded)
+    test_encoded = pd.DataFrame(data=test_ohe,
+                               columns=encoded_values, index=test.index)
+    test = test.join(test_encoded)
+    return train, test
+
+
+# Scale data with Standard Scaler
+
+def standard_scaler(train, test):
+    scaler = StandardScaler(copy=True, with_mean=True, with_std=True).fit(train)
+    train_scaled = pd.DataFrame(scaler.transform(train), columns=train.columns.values).set_index([train.index.values])
+    test_scaled = pd.DataFrame(scaler.transform(test), columns=test.columns.values).set_index([test.index.values])
+    return scaler, train_scaled, test_scaled
