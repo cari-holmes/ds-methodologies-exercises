d18ee6566ff32baeffdb2923c093624bc21bd10f
cariholmes <holmes.l.cari@gmail.com>
2019-11-17 21:44:34 -0600

    functions

---
+++ time_series/prepare.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import acquire
+
+
+def prepare_sale():
+    df = acquire.get_all_data(use_cache=True)
+    df['sale_date'] = pd.to_datetime(df['sale_date'])
+    df = df.sort_values(by='sale_date').set_index('sale_date')
+    df['month'] = df.index.strftime('%m-%b')
+    df['day_of_week'] = df.index.strftime('%w-%A')
+    df['total_sales'] = df['sale_amount'] * df['item_price']
+    sales_sum = df.resample("D")[['total_sales']].sum()
+    sales_sum['sales_differences'] = sales_sum['total_sales'].diff()
+    return df
+
+
+def sale_distributions():
+    df = acquire.get_all_data(use_cache=True)
+    df[['sale_amount']].plot()
+    df[['item_price']].plot()
+    df[['sale_date']].plot()
+
+
+
+df[['sale_amount']].plot()
+df[['item_price']].plot()
+df[['sale_date']].plot()
+
+
+def prepare_ops():
+    ops = acquire.get_opsd_data(use_cache=True)
+    ops['Date'] = pd.to_datetime(ops['Date'])
+    ops = ops.set_index('Date')
+    ops['month'] = ops.index.month
+    ops['year'] = ops.index.year
+    return ops
+
+
+def ops_distributions():
+    ops = acquire.get_opsd_data(use_cache=True)
+    ops['Date'] = pd.to_datetime(ops['Date'])
+    ops[['Consumption']].plot()
+    ops[['Wind']].plot()
+    ops[['Solar']].plot()
+    ops[['Wind+Solar']].plot()
+
