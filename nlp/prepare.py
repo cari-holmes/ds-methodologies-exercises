9d9c9c43dca9bb5852b2f691f97410c7fbf36882
cariholmes <holmes.l.cari@gmail.com>
2019-12-09 15:23:56 -0600

    updated funtions

--- nlp/prepare.py
+++ nlp/prepare.py
@@ -31,3 +31,28 @@ def lemmatize(text):
     text_lemmatized = ' '.join(lemmas)
     return text_lemmatized
 
+def remove_stopwords(string, extra_words=[], exclude_words=[]):
+    # Tokenize the string
+    string = tokenize(string)
+    words = string.split()
+    stopword_list = stopwords.words('english')
+    # Remove the excluded words from the stopword list 
+    stopword_list = set(stopword_list) - set(exclude_words)
+    # Add in user specified extra words
+    stopword_list = stopword_list.union(set(extra_words))
+    filtered_words = [w for w in words if w not in stopword_list ]
+    final_string = " ".join(filtered_words)
+    return final_string  
+
+def prep_article(df):
+    df['original'] = df.body
+    df['stemmed'] = df.body.apply(basic_clean).apply(stem)
+    df['lemmatized'] = df.body.apply(basic_clean).apply(lemmatize)
+    df['clean'] = df.body.apply(basic_clean).apply(remove_stopwords)
+    df.drop(columns=('body'), inplace=True)
+    return df
+# have to drop unnamed column separately
+
+def prepare_article_data():
+    df = acquire.get_blog_post()
+    return prep_article(df)
